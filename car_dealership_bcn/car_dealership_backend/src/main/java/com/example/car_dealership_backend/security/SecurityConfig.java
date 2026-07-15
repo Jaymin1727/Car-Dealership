8aad9898dd183b61feb415aacbf843f4983c0862
Jaymin Halpati <jayminhalpati548@gmail.com>
2026-07-15 18:46:53 +0530

    Configure CORS for Vercel frontend

--- car_dealership_bcn/car_dealership_backend/src/main/java/com/example/car_dealership_backend/security/SecurityConfig.java
+++ car_dealership_bcn/car_dealership_backend/src/main/java/com/example/car_dealership_backend/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173,car-dealership-jhmv.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control", "Accept"));
         configuration.setAllowCredentials(true);
